4d2372fdffca2b9f87ffcbecbfecdf3fa04ca4ce
Unknown <osher.filter@gmail.com>
2018-08-21 21:04:31 +0300

    front-slim - savepoint. dispair :(

--- front-slim/src/lab-receipt.js
+++ front-slim/src/lab-receipt.js
@@ -9,12 +9,13 @@ import { tag, template, useShadow } from 'slim-js/Decorators';
     <button
       bind:can-duplicate="canDuplicate(data, receipt)"
       click="duplicateItem"
-      role="duplicate"
     >+1</button>
     <button click="removeItem" bind:data="data">X</button>
     <lab-prod-effects
+      bind:receipt="receipt"
+      bind:can-duplicate="canDuplicate(data, receipt)"
       bind:main-effect="data.mainEffect"
-      bind:effects="data.effects"
+      bind:effects="appliedEffects(data, canDuplicate)"
     ></lab-prod-effects>
   </li>
   <li s:repeat="receipt.specials">
@@ -52,10 +53,16 @@ class LabEffects extends Slim {
       return 0 == (products.length + specials.length)
   }
 
-  canDuplicate (item) {
+  canDuplicate(item) {
     return this.receipt.products
       .filter( ({id}) => id === item.id )
       .length < 2
+  }
+  
+  appliedEffects(item) {
+      return this.canDuplicate(item)
+        ? item.effects
+        : [item.mainEffect]
   }
 
   duplicateItem ({target}) {
@@ -70,20 +77,69 @@ class LabEffects extends Slim {
   }
 }
 
+@tag('lab-product')
+@useShadow(true)
+@template(`
+    <span>{{product.name}}</span>
+    <button
+      bind:can-duplicate="canDuplicate(product, receipt)"
+      click="duplicateItem"
+    >+1</button>
+    <button click="removeItem" bind:product="product">X</button>
+    <div role="effects">
+      <span
+        s:repeat="effects(product, receipt) as effect"
+        bind:role="roleOf(effect)"
+      >{{effect.name}}</span>
+    </div>
+`)
+class LabProduct extends Slim {
+  product;
+  receipt;
+  onRemove;
+  
+  effects(product, receipt) {
+      return receipt.products.filter(({id}) => id == product.id).length > 1
+        ? [ product.mainEffect ]
+        : product.effects
+  }
+
+  removeItem ({target}) {
+    const { product } = target;
+    this.callAttribute('on-remove', data);
+  }
+}
+
 @tag('lab-prod-effects')
 @useShadow(true)
 @template(`
   <style>
-    li[role="main"] { font-weight: bold }
+    div {
+      display: flex;
+      flex-direction: row;
+    }
+    span[role="main"] { font-weight: bold }
+    span {
+      margin: 3px;
+      padding: 2px;
+      color: darkgray;
+      border: 1px solid darkgray;
+      display: inline-block;
+      border-radius: 2px;
+    }
+    
   </style>
-  <ol>
-    <li s:repeat="effects" bind:role="roleOf(data)">{{data.name}}</li>
-  </ol>
+  <div>
+    <span
+      s:repeat="effects"
+      bind:role="roleOf(data)"
+    >{{data.name}}</span>
+  </div>
 `)
-class LabProdEffects extends Slim{
-  prod;
+class LabProdEffects extends Slim {
   mainEffect;
   effects;
+  canDuplicate;
 
   roleOf(data) {
     return this.mainEffect.id == data.id ? "main" : "sec"
