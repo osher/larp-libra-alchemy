c54e412867b13d3a06f6de07640d653a691d5e91
Unknown <osher.filter@gmail.com>
2018-08-22 22:03:33 +0300

    front-vue - indicate similar + start handle post

--- front-vue/src/app.vue
+++ front-vue/src/app.vue
@@ -1,30 +1,3 @@
-<template>
-  <div id="app">
-    <LabIndex 
-      @on-selection="indexSelection"
-    />
-    <div id="content">
-      <Search 
-        ref="search"
-        @product="addProduct"
-        @special-ingr="addSpecialIngredient"
-      />
-      <Receipt
-        :products="products"
-        :specials="specials"
-        @drop="drop"
-        @duplicate="duplicate"
-      />
-      <Results 
-        :effects="effects"
-      />
-      <Similar
-        :potions="potions"
-      />
-    </div>
-  </div>
-</template>
-
 <style lang="scss">
 body { padding: 0 ; margin: 0; overflow: hidden }
 #app {
@@ -32,16 +5,94 @@ body { padding: 0 ; margin: 0; overflow: hidden }
   flex-direction: row;
   font-family: Arial;
 }
-
+#index {
+  display: flex;
+  min-width: 20%;
+}
 #content {
   display: flex;
-  flex: 3;
-  padding: 20px;
+  flex: auto;
   flex-direction: column;
-  height: 95vh;
+  height: 100vh;
+}
+#search {
+  height: 40px;
+}
+#workspace {
+  padding: 0px 20px 40px 20px;
+  flex: 1;
   overflow-y: auto;
 }
+#results {
+  display: flex;
+  flex-direction: row;
+}
+#results .h {
+  flex: 1;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
+#results #saveForm {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  margin-top: 20px;
+}
+#saveForm INPUT {
+  flex-grow: 1
+} 
+#results TEXTAREA {
+  width: 100%;
+  height: 100px;
+}
 </style>
+
+
+<template>
+  <div id="app">
+    <div id="index">
+      <LabIndex 
+        @on-selection="indexSelection"
+      />
+    </div>
+    <div id="content">
+      <div id="search">
+      <Search 
+        ref="search"
+        @product="addProduct"
+        @special-ingr="addSpecialIngredient"
+      />
+      </div>
+      <div id="workspace">
+        <Receipt
+          :products="products"
+          :specials="specials"
+          @drop="drop"
+          @duplicate="duplicate"
+        />
+        <div id="results">
+          <div class="h">
+            <Results 
+              :effects="effects"
+            />
+            <div v-if="effects.length && !potions.source" id="saveForm">
+              <input v-model="pubName"/><button @click="publish">פרסם</button>
+              <textarea v-model="pubDescr"></textarea>
+              יוצר: <input v-model="pubCreator"/>
+            </div>
+          </div>
+          <div class="h">
+            <Similar
+              :potions="potions"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 import Search from './components/search.vue'
@@ -63,7 +114,10 @@ export default {
       step: 0,
       products: r.products,
       specials: r.specials,
-      receipt: r
+      receipt: r,
+      pubName: '',
+      pubDescr: '',
+      pubCreator: '',
     }
   },
 
@@ -107,6 +161,46 @@ export default {
     },
     indexSelection(item) {
       this.$refs.search.select(item)
+    },
+    publish() {
+      const { 
+        pubName: name,
+        pubDescr: descr,
+        pubCreator: by,
+        effects 
+      } = this;
+
+      const data = { 
+        name,
+        by,
+        descr,
+        effects:  
+          effects.map(
+            ({level, effect: { id: effectId }}) => ({effectId, level})
+          )
+      };
+      
+      console.log('publish clicked', data)
+      
+      const reject =
+        !name && "יש להזין שם שיקוי"
+        || descr.length < 20 && "תיאור השיקוי קצר מדיי"
+        || !by && "יש להזין את שם דמות יוצר השיקוי"
+      ;
+      if (reject) {
+          return alert(reject);
+      }
+      
+      //TBD: indicate sending with loader
+      fetch('http://localhost:3030/potion', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(() => { /* indicate success */ })
+      .catch((e) => { /* indicate error */ })
     }
   }
  }
