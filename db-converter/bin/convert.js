de692b90f0ec6d94500295a250540b9274f33930
Unknown <osher.filter@gmail.com>
2018-09-04 21:21:27 +0300

    db-converter - add dist + npm run build

--- db-converter/bin/convert.js
+++ db-converter/bin/convert.js
@@ -1,7 +1,7 @@
 const src = process.argv[2];
 if (!src) return console.error('Error: no source\nplease provide the path to the source for conversion\n\n');
 
-const trg = process.argv[3] || process.argv[2] + '.converted.sql'
+const trg = (process.argv[3] || process.argv[2] + '.converted.sql').replace('data', 'dist')
 const txt = require('../lib')({src, trg})
 
-console.log('converted from %s to %s, total %s statements', src, trg, txt.split(';').length);
+console.log('converted\n\tfrom:\t%s\n\tto:\t%s\n\ttotal:\t%s statements', src, trg, txt.split(';').length);
